ebbe928b6bb0f5920c553465babc7f3b251f23ee
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2022-07-21 21:13:28 +0200

    Refactor search result content generation

--- src/components/TranslateFromDetectedLanguage.tsx
+++ src/components/TranslateFromDetectedLanguage.tsx
@@ -1,5 +1,5 @@
 import { List } from "@raycast/api";
-import { Language, languages, useTranslation } from "../lib/deeplapi";
+import { Language, languages, Translation, Usage, useTranslation } from "../lib/deeplapi";
 import TranslationResultListItem from "./TranslationResultListItem";
 
 export default function TranslateFromDetectedLanguage(props: { targetLanguage: Language }) {
@@ -8,28 +8,6 @@ export default function TranslateFromDetectedLanguage(props: { targetLanguage: L
   // TODO: Show dropdown of languages to translate from, defaulting to autodetected language
   // TODO: Only show usage when on Free plan
   // TODO: Only show formality when on Pro plan
-  let subtitle: string;
-
-  if (state.usage != null) {
-    const usagePercentage = Number(state.usage.character_count / state.usage.character_limit).toLocaleString(
-      undefined,
-      {
-        style: "percent",
-        maximumFractionDigits: 2,
-      }
-    );
-    subtitle = `${state.usage.character_count}/${state.usage.character_limit} characters used (${usagePercentage})`;
-  } else {
-    subtitle = "";
-  }
-
-  const languageCode: string = state.translation?.detected_source_language ?? "";
-  const sourceLanguage = languageCode
-    ? languages.find((language: Language) => language.code == languageCode)
-    : null;
-  const title = sourceLanguage
-    ? `Translated from ${sourceLanguage?.name}:`
-    : "Translation:"
 
   return (
     <List
@@ -38,9 +16,43 @@ export default function TranslateFromDetectedLanguage(props: { targetLanguage: L
       searchBarPlaceholder={`Translate to ${props.targetLanguage.name} using DeepL…`}
       throttle
     >
-      <List.Section title={title} subtitle={subtitle}>
+      <List.Section
+        title={generateTitle(state.translation)}
+        subtitle={generateSubtitle(state.usage)}
+      >
         <TranslationResultListItem state={state} />
       </List.Section>
     </List>
   );
+}
+
+function generateTitle(translation: Translation | null): string {
+  if (translation == null) {
+    return "Translation:";
+  }
+
+  const sourceLanguage = translation.detected_source_language
+    ? languages.find((language: Language) => language.code == translation.detected_source_language)
+    : null;
+
+  if (sourceLanguage == null) {
+    return "Translation:";
+  }
+
+  return `Translated from ${sourceLanguage.name}:`;
+}
+
+function generateSubtitle(usage: Usage | null): string {
+  if (usage == null) {
+    return "";
+  }
+
+  const usagePercentage = Number(usage.character_count / usage.character_limit).toLocaleString(
+    undefined,
+    {
+      style: "percent",
+      maximumFractionDigits: 2,
+    }
+  );
+  return `${usage.character_count}/${usage.character_limit} characters used (${usagePercentage})`;
 }
