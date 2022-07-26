ea47397f776d71e9c070c4f7843c58ec2ddb9a5e
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2022-07-26 22:45:52 +0200

    Move from node-fetch to useFetch and refactor everything

--- src/command.tsx
+++
@@ -1,6 +0,0 @@
-import { Language } from "./lib/deeplapi";
-import TranslationResultList from "./components/TranslationResultList";
-
-export default function command(targetLanguage: Language): () => JSX.Element {
-  return () => <TranslationResultList targetLanguage={targetLanguage} />;
-}
--- src/components/TranslationResultListItem.tsx
+++ src/components/TranslationResultListItem.tsx
@@ -1,27 +1,44 @@
-import { Action, ActionPanel, Icon, List, useNavigation } from "@raycast/api";
-import { TranslationState } from "../lib/deeplapi";
+import { Action, ActionPanel, getPreferenceValues, Icon, List, useNavigation } from "@raycast/api";
+import { Translation, Usage } from "../deepl-api";
 import TranslationResultDetail from "./TranslationResultDetail";
 
-export default function TranslationResultListItem({ state }: { state: TranslationState }) {
-  if (state.translation == null) {
-    return null;
-  }
-
+export default function TranslationResultListItem(props: { translation: Translation; usage: Usage | undefined }) {
   const { push } = useNavigation();
+  const title = `Translated from ${props.translation.detectedSourceLanguage.name}:`;
+  const subtitle = props.usage ? generateSubtitle(props.usage) : "";
 
   return (
-    <List.Item
-      title={state.translation.text}
-      actions={
-        <ActionPanel>
-          <Action
-            title="View Translation"
-            icon={{ source: Icon.AppWindowSidebarRight }}
-            onAction={() => push(<TranslationResultDetail state={state} />)}
-          />
-          <Action.CopyToClipboard title="Copy Translated Text" content={state.translation?.text} />
-        </ActionPanel>
-      }
-    />
+    <List.Section title={title} subtitle={subtitle}>
+      <List.Item
+        title={props.translation.text}
+        actions={
+          <ActionPanel>
+            <Action
+              title="View Translation"
+              icon={{ source: Icon.AppWindowSidebarRight }}
+              onAction={() => push(<TranslationResultDetail translation={props.translation} usage={props.usage} />)}
+            />
+            <Action.CopyToClipboard title="Copy Translated Text" content={props.translation.text} />
+          </ActionPanel>
+        }
+      />
+    </List.Section>
   );
+}
+
+function generateSubtitle(usage: Usage | null): string {
+  if (usage == null) {
+    return "";
+  }
+
+  const usagePercentage = (usage.characterCount / usage.characterLimit).toLocaleString(undefined, {
+    style: "percent",
+    maximumFractionDigits: 2,
+  });
+
+  const characterCount = usage.characterCount.toLocaleString();
+  const characterLimit = usage.characterLimit.toLocaleString();
+  const plan = getPreferenceValues().plan == "free" ? "DeepL API Free" : "DeepL API Pro";
+
+  return `${usagePercentage} of ${plan} plan used this period (${characterCount} / ${characterLimit} characters)`;
 }
